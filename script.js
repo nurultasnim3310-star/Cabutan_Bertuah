b2a5571f76461083b3084681c07e520ef2b36355
nurultasnim3310-star <nurultasnim3310@gmail.com>
2026-06-08 11:17:14 +0800

    edit all

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 $(document).ready(function(){
 
-    var peserta = [
-        "Tasnim",
-        "Natasha",
-        "Haikal",
-        "Adib",
-        "Qyra",
-        "Farhan",
-        "Alya",
-        "Nadiah",
-        "Suri",
-        "Ain"
-    ];
+    var peserta = [];
 
-    $("#draw").click(function(){
+    // Simpan nama peserta
+    $("#tambah").click(function(){
+
+        var nama = $("#nama").val();
+
+        if(nama != ""){
+            peserta.push(nama);
+
+            $("#senarai").append("<li>" + nama + "</li>");
+
+            $("#nama").val("");
+        }
+    });
+
+    
+    $("#pemenang").click(function(){
+
+        if(peserta.length == 0){
+            $("#winner").text("Tiada peserta!");
+            return;
+        }
 
         var random = Math.floor(Math.random() * peserta.length);
 
-        $("#winner").text("🎉 Pemenang: " + peserta[random]);
-
+        $("#winner").text(
+            "🎉 Pemenang: " + peserta[random]
+        );
     });
 
 });
